b9046d2f86842845707c6d4936cf1af2629ded75
KevC000 <chenkevin1119@gmail.com>
2023-07-04 08:01:07 -0400

    LC74

--- LeetCode/015-LC150.py
+++ LeetCode/015-LC150.py
@@ -1,5 +1,8 @@
 #Evaluate Reverse Polish Notation
 
+from ast import List
+
+
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
@@ -20,5 +23,6 @@ class Solution:
 
 # Time O(N) Space O(N)
 # We want to perform the operations on the most recent 2 numbers which we can keep track of with a stack.
-# Iterate through the tokens, if it is an operator we perform the operations on the top 2 items in the stack and append the result which where we will perform more operations as we iterate throught the tokens.
+# Iterate through the tokens, if it is an operator we perform the operations on the top 2 items in the stack and append the result.
+# We append the result in order to perform future operations to it.
 # If we have a number we append that to the stack to be used later.
---
+++ LeetCode/016-LC74.py
@@ -0,0 +1,47 @@
+# Search a 2D Matrix
+from ast import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row_start = 0
+        row_end = len(matrix)-1
+
+        
+        while row_start <= row_end:
+            mid = (row_start + row_end)//2
+            if target > matrix[mid][-1]:
+                row_start = mid+1
+            elif target < matrix[mid][0]:
+                row_end = mid-1
+            else:
+                break
+        
+        if row_start > row_end: return False
+
+        row = (row_start + row_end)//2
+        col_start, col_end = 0, len(matrix[0])-1
+
+        while col_start <= col_end:
+            mid = (col_start+col_end)//2
+            if target > matrix[row][mid]:
+                col_start = mid+1
+            elif target < matrix[row][mid]:
+                col_end = mid-1
+            else:
+                return True
+        return False
+
+# Thought process:    
+# Time O(log(m * n) Space O(1)
+# To solve this is log time we need to perform binary search. 
+# How do we perform binary search on matrix/2D array? First find the row it belongs to then the column.
+# Get the mid row if the target is greater than the greatest value in the mid row we look to the higher half of the rows, and lower rows if it's less than the lowest.
+# If it's not in any row we return False.
+# Once you find the row it's just regulary bianry search :). Return True if it's in that row if not return False.
+
+# Self-Analysis:
+# I knew it was a binary search problembecause of the tag on NC150, but wasn't sure how it could be applied to a matrix, so I thought of alternatives.
+# First I thought of using two pointers twice instead of binary serch but I realized it would be N+M time.
+# Then I realized I could split the matrix in two halves by rows then by columns and perform bianry search twice.
+# Conceptually I understodd by coding it up was a bit tricky for me but I did it :).
